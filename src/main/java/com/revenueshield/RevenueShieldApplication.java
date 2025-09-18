2471cbaf9ce61667f670b74792bb9a3977657d32
Abhishek Kumar <abhishekkumar@Abhisheks-MacBook-Pro.local>
2025-09-18 21:42:36 +0530

    initial sample project

---
+++ src/main/java/com/revenueshield/RevenueShieldApplication.java
@@ -0,0 +1,11 @@
+package com.revenueshield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevenueShieldApplication{
+    public static void main(String[] args){
+        SpringApplication.run(RevenueShieldApplication.class, args);
+    }
+}
